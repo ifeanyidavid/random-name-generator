3f3b037ce7b428f95c3b5f031b56b1a112b94244
Dave Kalu <davekalu46@gmail.com>
2019-12-13 21:43:30 +0100

    Updates

---
+++ build/precache-manifest.284ba0f4c04f32d6a184e765c81e3747.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e14000aeea378b6da432dda660891fcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "badd238ae6804e2e73e4",
+    "url": "/static/css/main.1b30939a.chunk.css"
+  },
+  {
+    "revision": "aac0634f5b05537fdbab",
+    "url": "/static/js/2.9679eab4.chunk.js"
+  },
+  {
+    "revision": "badd238ae6804e2e73e4",
+    "url": "/static/js/main.c92ad776.chunk.js"
+  },
+  {
+    "revision": "fc71d79245ec34aa3806",
+    "url": "/static/js/runtime-main.8140d594.js"
+  }
+]);
